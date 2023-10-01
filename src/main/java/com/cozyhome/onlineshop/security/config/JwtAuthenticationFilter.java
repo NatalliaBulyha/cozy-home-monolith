5268dcbdb717a892b2cab806d94fc72e52433b28
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-10-01 14:14:51 +0200

    added filter

--- src/main/java/com/cozyhome/onlineshop/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/cozyhome/onlineshop/security/config/JwtAuthenticationFilter.java
@@ -4,13 +4,34 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.net.http.HttpHeaders;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final String AUTHORIZATION = "Authorization";
+    private final String BEARER = "Bearer ";
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NotNull HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    @NotNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader(AUTHORIZATION);
+        final String jwt;
+
+        if (authHeader == null || !authHeader.startsWith(BEARER)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(BEARER.length());
 
     }
 }
