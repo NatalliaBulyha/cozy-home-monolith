aeb5167d3a67bec82c8898f09e2bcc4b4a0f65fb
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-10-08 18:23:53 +0200

    change config class

--- src/main/java/com/cozyhome/onlineshop/config/WebSecurityConfig.java
+++ src/main/java/com/cozyhome/onlineshop/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.cozyhome.onlineshop.config;
 
 import com.cozyhome.onlineshop.userservice.security.JWT.JwtTokenFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
@@ -31,9 +33,9 @@ public class WebSecurityConfig {
 	@Value("${api.basePath}/**")
 	private String GENERAL_ACCESS_URL;
 	private static final String JWT_TOKEN_EXPIRED = "/api/v1/auth/expired-jwt";
-	private static final String[] SWAGGER_URL = {
-			"/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html"
-	};
+
+	@Value("${api.auth.whitelist}")
+	private final String[] AUTH_WHITELIST;
 
 	@Bean
 	public JwtTokenFilter authenticationJwtTokenFilter() {
@@ -63,7 +65,7 @@ public class WebSecurityConfig {
 		http.csrf(csrf -> csrf.disable())
 				.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(auth -> auth.requestMatchers(GENERAL_ACCESS_URL, JWT_TOKEN_EXPIRED).permitAll()
+				.authorizeHttpRequests(auth -> auth.requestMatchers(AUTH_WHITELIST).permitAll()
 						.anyRequest().authenticated());
 
 		http.authenticationProvider(authenticationProvider());
