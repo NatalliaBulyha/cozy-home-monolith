192670c581b09ff36cfc7289fca68e164c9b2a13
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-10-09 23:16:53 +0200

    CH-544/CH-634 oldPassword and newPassword are optional fields
    jira [endpoint: change the user's personal data] https://cozyhometeam.atlassian.net/browse/CH-634?atlOrigin=eyJpIjoiYjY5NzE2NGU0NjIxNDcxMThjMjc4MzlhZWM1N2JhMzgiLCJwIjoiaiJ9

---
+++ src/main/java/com/cozyhome/onlineshop/validation/ValidConditionalPassword.java
@@ -0,0 +1,28 @@
+package com.cozyhome.onlineshop.validation;
+
+import com.cozyhome.onlineshop.validation.impl.ConditionalPasswordConstraintValidator;
+import com.cozyhome.onlineshop.validation.impl.EmailConstraintValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = ConditionalPasswordConstraintValidator.class)
+@Target({ TYPE, FIELD, ANNOTATION_TYPE, PARAMETER })
+@Retention(RUNTIME)
+public @interface ValidConditionalPassword {
+    String message() default "Invalid password.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
