f1a85f07fc1be81ca3b643603c6de513a2b42f41
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-10-13 17:00:43 +0200

    CH-444/CH-457 changed the logic of building the ErrorResponse
    jira [add exception handling where needed] https://cozyhometeam.atlassian.net/browse/CH-457?atlOrigin=eyJpIjoiZDZjZThmNjhiNmUyNDZhMWI1YzE0MDY5MmIxNDdiZjciLCJwIjoiaiJ9

--- src/main/java/com/cozyhome/onlineshop/handler/CozyHomeExceptionHandler.java
+++ src/main/java/com/cozyhome/onlineshop/handler/CozyHomeExceptionHandler.java
@@ -1,19 +1,22 @@
 package com.cozyhome.onlineshop.handler;
 
-import com.cozyhome.onlineshop.exception.AuthenticationException;
+import com.cozyhome.onlineshop.exception.AuthException;
 import com.cozyhome.onlineshop.exception.DataNotFoundException;
 import com.cozyhome.onlineshop.exception.InvalidTokenException;
 import io.jsonwebtoken.JwtException;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.UnexpectedTypeException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.validation.BindingResult;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -24,58 +27,89 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 @Slf4j(topic = "COZY_HOME_EXCEPTION_HANDLER")
 @RestControllerAdvice
 public class CozyHomeExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({DataNotFoundException.class, IllegalArgumentException.class})
+    public static final String TRACE = "trace";
+
+    @Value("${reflectoring.trace:false}")
+    private boolean printStackTrace;
+
+    @ExceptionHandler({DataNotFoundException.class, IllegalArgumentException.class, NoSuchElementException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleDataNotFoundException(Exception ex) {
-        return bodyBuilder(ex.getMessage());
+    public ResponseEntity<Object> handleDataNotFoundException(Exception ex, WebRequest request) {
+        return buildErrorResponse(ex, ex.getMessage(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler({ConstraintViolationException.class, UnexpectedTypeException.class, MethodArgumentTypeMismatchException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleValidationException(Exception ex) {
-        return bodyBuilder(ex.getMessage());
+    public ResponseEntity<Object> handleValidationException(Exception ex, WebRequest request) {
+        return buildErrorResponse(ex, ex.getMessage(), HttpStatus.BAD_REQUEST, request);
     }
 
-    @ExceptionHandler({BadCredentialsException.class, AuthenticationException.class})
+    @ExceptionHandler({BadCredentialsException.class, AuthenticationException.class, AuthException.class})
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ErrorResponse handleAuthenticationException(Exception ex) {
-        return bodyBuilder(ex.getMessage());
+    public ResponseEntity<Object> handleAuthenticationException(Exception ex, WebRequest request) {
+        return buildErrorResponse(ex, ex.getMessage(), HttpStatus.UNAUTHORIZED, request);
     }
 
     @ExceptionHandler({AccessDeniedException.class, JwtException.class, InvalidTokenException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ErrorResponse handleForbiddenException(Exception ex) {
-        return bodyBuilder(ex.getMessage());
-    }
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        BindingResult bindingResult = ex.getBindingResult();
-        FieldError fieldError = bindingResult.getFieldError();
-        assert fieldError != null;
-        String defaultMessage = fieldError.getDefaultMessage();
-        return new ResponseEntity<>(bodyBuilder(defaultMessage), status);
+    public ResponseEntity<Object> handleForbiddenException(Exception ex, WebRequest request) {
+        return buildErrorResponse(ex, ex.getMessage(), HttpStatus.FORBIDDEN, request);
     }
 
     @Override
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status,
+                                                                  WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Validation error. Check 'errors' field for details.");
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errorResponse.addValidationError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.unprocessableEntity().body(errorResponse);
+    }
+
+    @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
                                                                           HttpHeaders headers, HttpStatusCode status,
                                                                           WebRequest request) {
-        String defaultMessage = ex.getMessage();
-        return new ResponseEntity<>(bodyBuilder(defaultMessage), status);
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+                ex.getParameterName() + " parameter is missing.");
+        return ResponseEntity.unprocessableEntity().body(errorResponse);
     }
 
-    private ErrorResponse bodyBuilder(String message) {
-        ErrorResponse body = new ErrorResponse();
-        body.setMessage(message);
-        body.setTimestamp(LocalDateTime.now());
-        log.error(message);
-        return body;
+    @Override
+    public ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
+                                                          HttpStatusCode statusCode, WebRequest request) {
+
+        return buildErrorResponse(ex, statusCode, request);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception exception, HttpStatusCode httpStatus,
+                                                      WebRequest request) {
+        return buildErrorResponse(exception, exception.getMessage(), httpStatus, request);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception exception, String message, HttpStatusCode httpStatus,
+                                                      WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), message);
+        if (printStackTrace && isTraceOn(request)) {
+            errorResponse.setStackTrace(ExceptionUtils.getStackTrace(exception));
+        }
+        return ResponseEntity.status(httpStatus).body(errorResponse);
+    }
+
+    private boolean isTraceOn(WebRequest request) {
+        String[] value = request.getParameterValues(TRACE);
+        return Objects.nonNull(value)
+                && value.length > 0
+                && value[0].contentEquals("true");
     }
 }
