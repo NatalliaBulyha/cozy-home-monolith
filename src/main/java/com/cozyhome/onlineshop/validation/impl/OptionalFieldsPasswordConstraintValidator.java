b9435b3aaa76136faef098125f01454a761e5ce4
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-10-13 23:55:36 +0200

    CH-444/CH-669 changed valid logic
    jira [simplification of validation for changing the password in the personal account] https://cozyhometeam.atlassian.net/browse/CH-669?atlOrigin=eyJpIjoiZmMwNDg2MmU3MGZjNGJlZjk2MjQ1ZmYzZGI2ZDdlYmUiLCJwIjoiaiJ9

--- src/main/java/com/cozyhome/onlineshop/validation/impl/OptionalFieldsPasswordConstraintValidator.java
+++ src/main/java/com/cozyhome/onlineshop/validation/impl/OptionalFieldsPasswordConstraintValidator.java
@@ -6,13 +6,13 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class OptionalFieldsPasswordConstraintValidator implements ConstraintValidator<ValidOptionalFieldsPassword, UserInformationRequest> {
+    private final String regex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+
     @Override
     public void initialize(ValidOptionalFieldsPassword constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
     }
-
-    @Override
-    public boolean isValid(UserInformationRequest userInformationRequest, ConstraintValidatorContext constraintValidatorContext) {
+    /*public boolean isValid2(UserInformationRequest userInformationRequest, ConstraintValidatorContext constraintValidatorContext) {
         if (userInformationRequest.getNewPassword() != null && !userInformationRequest.getNewPassword().isEmpty()
         && userInformationRequest.getOldPassword() != null && !userInformationRequest.getOldPassword().isEmpty()) {
             return userInformationRequest.getNewPassword()
@@ -27,6 +27,18 @@ public class OptionalFieldsPasswordConstraintValidator implements ConstraintVali
             return false;
         } else {
             return true;
+        }
+    }*/
+
+    @Override
+    public boolean isValid(UserInformationRequest request, ConstraintValidatorContext constraintValidatorContext) {
+        if (request.getOldPassword() == null && request.getNewPassword() == null) {
+            return true;
+        } else if (request.getOldPassword() != null && !request.getOldPassword().isEmpty()
+                && request.getNewPassword() != null && !request.getNewPassword().isEmpty()) {
+            return request.getNewPassword().matches(regex) && request.getOldPassword().matches(regex);
+        } else {
+            return false;
         }
     }
 }
