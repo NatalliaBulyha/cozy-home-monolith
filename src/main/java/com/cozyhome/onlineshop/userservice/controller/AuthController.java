ea2554c29c94c50467a604ec9242100b77f17e02
Daria Bohush <d.bogoosh@gmail.com>
2023-10-13 16:00:39 +0200

    add dto

--- src/main/java/com/cozyhome/onlineshop/userservice/controller/AuthController.java
+++ src/main/java/com/cozyhome/onlineshop/userservice/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.cozyhome.onlineshop.userservice.controller;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cozyhome.onlineshop.dto.TokenResponseDto;
 import com.cozyhome.onlineshop.dto.auth.EmailRequest;
 import com.cozyhome.onlineshop.dto.auth.LoginRequest;
 import com.cozyhome.onlineshop.dto.auth.MessageResponse;
@@ -50,19 +50,18 @@ public class AuthController {
 	private final SecurityTokenService securityTokenService;
 
 	private final String emailErrorMessage = "Error: Email is already in use!";
-	private final String registrationSuccessMessage = "User registered successfully!";
 
 	@Operation(summary = "Existing user login", description = "Allows an existing user to log in using his email and password.")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = SwaggerResponse.Code.CODE_200, description = SwaggerResponse.Message.CODE_200_FOUND_DESCRIPTION) })
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody @Valid LoginRequest loginRequest) {
+	public ResponseEntity<TokenResponseDto> login(@RequestBody @Valid LoginRequest loginRequest) {
 		String username = loginRequest.getUsername();
 		boolean isAuthenticated = securityService.isAuthenticated(username, loginRequest.getPassword());
 
 		if (isAuthenticated) {
 			String token = jwtTokenUtil.generateToken(username);
-			return ResponseEntity.ok().body(token);
+			return ResponseEntity.ok().body(new TokenResponseDto(token));
 		} else {
 			log.warn("[ON login]:: Authentication failed for user: {}", username);
 			throw new AuthenticationException("Authentication failed for user");
@@ -79,37 +78,37 @@ public class AuthController {
 			return ResponseEntity.badRequest().body(new MessageResponse(emailErrorMessage));
 		}
 		userService.saveUser(signUpRequest);
-		return ResponseEntity.ok(new MessageResponse(registrationSuccessMessage));
+		return ResponseEntity.ok().build();
 	}
 
 	@Operation(summary = "Activate e-mail.", description = "User follows the link sent to him during registration and activates his mail in this method")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = SwaggerResponse.Code.CODE_200, description = SwaggerResponse.Message.CODE_200_FOUND_DESCRIPTION) })
 	@GetMapping("/activate")
-	public ResponseEntity<MessageResponse> activateUser(@RequestParam @ValidUUID String activationToken) {
+	public ResponseEntity<TokenResponseDto> activateUser(@RequestParam @ValidUUID String activationToken) {
 		User activatedUser = userService.activateUser(activationToken);
 		String token = jwtTokenUtil.generateToken(activatedUser.getEmail());
-		return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, token).body(new MessageResponse("success"));
+		return ResponseEntity.ok().body(new TokenResponseDto(token));
 	}
 
 	@Operation(summary = "Send a password reset link to the user's email address", description = "Sends a password reset link to the user's email address if the user have forgotten his password.")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = SwaggerResponse.Code.CODE_200, description = SwaggerResponse.Message.CODE_200_FOUND_DESCRIPTION) })
 	@PostMapping("/login/forgot")
-	public ResponseEntity<MessageResponse> forgetPassword(@RequestBody @Valid EmailRequest emailRequest,
+	public ResponseEntity<Void> forgetPassword(@RequestBody @Valid EmailRequest emailRequest,
 			HttpServletRequest httpRequest) {
 		securityTokenService.createPasswordResetToken(emailRequest.getEmail(), httpRequest.getRemoteAddr());
-		return ResponseEntity.ok(new MessageResponse("success"));
+		return ResponseEntity.ok().build();
 	}
 
 	@Operation(summary = "Reset user password", description = "User follows the link previously sent to his e-mail and enters a new password")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = SwaggerResponse.Code.CODE_200, description = SwaggerResponse.Message.CODE_200_FOUND_DESCRIPTION) })
 	@PostMapping("/login/reset")
-	public ResponseEntity<MessageResponse> resetPassword(@RequestParam @ValidUUID String resetPasswordToken,
+	public ResponseEntity<TokenResponseDto> resetPassword(@RequestParam @ValidUUID String resetPasswordToken,
 			@RequestBody @Valid NewPasswordRequest newPassword) {
 		User user = userService.resetPassword(resetPasswordToken, newPassword);
 		String token = jwtTokenUtil.generateToken(user.getEmail());
-		return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, token).build();
+		return ResponseEntity.ok().body(new TokenResponseDto(token));
 	}
 }
