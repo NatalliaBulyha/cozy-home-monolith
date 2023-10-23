e6d7796594f8f57a65e529e882e477c8756b6dcb
Daria Bohush <d.bogoosh@gmail.com>
2023-10-23 21:33:50 +0200

    CH-699 add keepAlive method -> send request to the server every minute

---
+++ src/main/java/com/cozyhome/onlineshop/keepAliveServer/KeepAliveController.java
@@ -0,0 +1,20 @@
+package com.cozyhome.onlineshop.keepAliveServer;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/ping")
+public class KeepAliveController {
+	
+	@GetMapping()
+	public ResponseEntity<String> pingServer() {
+	    log.info("[ON pingServer] :: Received a keep-alive request at /api/v1/ping");
+	    return ResponseEntity.ok().build();
+	}
+}
