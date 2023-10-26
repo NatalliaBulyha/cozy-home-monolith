4a9d8b0259d8f71567b3822d98f405b535ee1ab0
Daria Bohush <d.bogoosh@gmail.com>
2023-10-26 21:56:55 +0200

    CH-689 Bag: non activated user can log in -> Fixed
    jira[Fix]:https://cozyhometeam.atlassian.net/browse/CH-689?atlOrigin=eyJpIjoiZGVjNDA4ODJhMGZkNGE3NGIzOGQ5NmUzNzlhMjI0YTAiLCJwIjoiaiJ9

--- src/main/java/com/cozyhome/onlineshop/userservice/repository/UserRepository.java
+++ src/main/java/com/cozyhome/onlineshop/userservice/repository/UserRepository.java
@@ -3,17 +3,15 @@ package com.cozyhome.onlineshop.userservice.repository;
 import java.util.List;
 import java.util.Optional;
 
-import com.cozyhome.onlineshop.userservice.model.RoleE;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.cozyhome.onlineshop.userservice.model.RoleE;
 import com.cozyhome.onlineshop.userservice.model.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, ObjectId> {
-
-	Optional<User> getByEmail(String email);
 
 	Optional<User> getUserById(String id);
 	
--- src/main/java/com/cozyhome/onlineshop/userservice/security/service/impl/SecurityServiceImpl.java
+++ src/main/java/com/cozyhome/onlineshop/userservice/security/service/impl/SecurityServiceImpl.java
@@ -1,11 +1,15 @@
 package com.cozyhome.onlineshop.userservice.security.service.impl;
 
-import com.cozyhome.onlineshop.userservice.security.service.SecurityService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.stereotype.Service;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+import com.cozyhome.onlineshop.exception.DataNotFoundException;
+import com.cozyhome.onlineshop.userservice.model.User;
+import com.cozyhome.onlineshop.userservice.repository.UserRepository;
+import com.cozyhome.onlineshop.userservice.security.service.SecurityService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,17 +20,25 @@ import lombok.extern.slf4j.Slf4j;
 public class SecurityServiceImpl implements SecurityService {
 
 	private final AuthenticationManager authenticationManager;
-	
+	private final UserRepository userRepository;
+
 	@Override
 	public boolean isAuthenticated(String username, String password) {
 		try {
 			log.info("[ON isAuthenticated]:: authenticating by UsernamePasswordAuthenticationToken...");
-	        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-	        log.info("[ON authenticate]:: user with username [ {} ] is authenticated successfully", username);
-            return true;
-        } catch (AuthenticationException e) {
-            log.error("[ON isAuthenticated]:: Exception :: {}.", e);
-            throw new BadCredentialsException(e.getMessage());
-        }
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+			log.info("[ON isAuthenticated]:: user with username [ {} ] is authenticated successfully", username);
+			return true;
+		} catch (AuthenticationException e) {
+			log.error("[ON isAuthenticated]:: Exception :: {}.", e);
+			throw new BadCredentialsException(e.getMessage());
+		}
+	}
+
+	@Override
+	public boolean isActivated(String username) {
+		User user = userRepository.findByEmail(username)
+				.orElseThrow(() -> new DataNotFoundException("No user found by username " + username));
+		return user.isActivated();
 	}
 }
