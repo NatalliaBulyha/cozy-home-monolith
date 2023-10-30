8aac5e0f01bbf15486aa1831af11e1ba2206cf57
Daria Bohush <d.bogoosh@gmail.com>
2023-10-30 21:27:58 +0100

    fix cross origin bag

--- src/main/java/com/cozyhome/onlineshop/userservice/controller/UserSecuredController.java
+++ src/main/java/com/cozyhome/onlineshop/userservice/controller/UserSecuredController.java
@@ -26,9 +26,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@CrossOrigin(origins = { "${api.front.base_url}", "${api.front.localhost}", "${api.front.test_url}",
-		"${api.front.additional_url}", "${api.front.main.url}" }, allowedHeaders = { "Authorization" },
-	    exposedHeaders = { "Access-Control-Allow-Methods" })
+//@CrossOrigin(origins = { "${api.front.base_url}", "${api.front.localhost}", "${api.front.test_url}",
+//		"${api.front.additional_url}", "${api.front.main.url}" }, allowedHeaders = { "Authorization" },
+//	    exposedHeaders = { "Access-Control-Allow-Methods" })
 @Tag(name = "User")
 @ApiResponse
 @RequiredArgsConstructor
