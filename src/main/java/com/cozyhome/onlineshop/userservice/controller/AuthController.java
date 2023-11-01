c1303f12fe317edb0ba6e7584fd6eaf7f5975a21
Daria Bohush <d.bogoosh@gmail.com>
2023-11-01 22:46:23 +0100

    add temporal url to cors

--- src/main/java/com/cozyhome/onlineshop/userservice/controller/AuthController.java
+++ src/main/java/com/cozyhome/onlineshop/userservice/controller/AuthController.java
@@ -35,7 +35,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @CrossOrigin({ "${api.front.base_url}", "${api.front.localhost}", "${api.front.test_url}",
-		"${api.front.additional_url}", "${api.front.main.url}" })
+		"${api.front.additional_url}", "${api.front.main.url}", "${api.front.temporal.url}" })
 @Tag(name = "Auth")
 @RequiredArgsConstructor
 @Slf4j
