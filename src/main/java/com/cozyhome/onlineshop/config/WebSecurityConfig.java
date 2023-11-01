1a4eea4a66fb1ef41d06c4a193e05c9baa2bc59a
Daria Bohush <d.bogoosh@gmail.com>
2023-11-01 19:05:14 +0100

    aboard changes

--- src/main/java/com/cozyhome/onlineshop/config/WebSecurityConfig.java
+++ src/main/java/com/cozyhome/onlineshop/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,7 +18,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -40,6 +38,8 @@ public class WebSecurityConfig {
 	private final UserDetailsServiceImpl userDetailsService;
 	private final JwtAuthEntryPoint unauthorizedHandler;
 	private final String logoutURL = "${api.secure.basePath}/user/logout";
+	private final List<String> allowedOrigins = List.of("${api.front.base_url}", "${api.front.localhost}", "${api.front.test_url}",
+		"${api.front.additional_url}", "${api.front.main.url}", "${api.front.temporal.url}");
 
 	@Value("${api.auth.whitelist}")
 	private final String[] AUTH_WHITELIST;
@@ -76,23 +76,24 @@ public class WebSecurityConfig {
 						auth -> auth.requestMatchers(AUTH_WHITELIST).permitAll().anyRequest().authenticated());
 
 		http.authenticationProvider(authenticationProvider());
-		http.addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class);
+//		http.addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class);
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);		
 		return http.build();
 	}
 	
-	@Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("*"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setExposedHeaders(List.of("Authorization"));
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsFilter(source);
-    }
+//	@Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowedOrigins(allowedOrigins);
+//        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+//        corsConfiguration.setAllowCredentials(true);
+//        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+//        source.registerCorsConfiguration("/api/v1/**", corsConfiguration);
+//        source.registerCorsConfiguration("/api-secure/v1/**", corsConfiguration);
+//        return new CorsFilter(source);
+//    }
 
 	@Bean
 	public SecurityFilterChain filterLogoutChain(HttpSecurity http) throws Exception {
